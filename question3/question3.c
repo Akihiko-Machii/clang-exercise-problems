879cb0f2b880fb3b683bbaaf965bda7432340899
Akihiko-Machii <Akihiko_Machii@sis2.saison.co.jp>
2024-10-04 11:08:15 +0900

    feat: question3を追加する

---
+++ question3/question3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    char str[] = "abcdefghijklmnopqrstu";
+    int len = strlen(str);
+
+    printf("配列サイズ=%d 文字列長=%d\n1文字目は%c、最後の文字は%cです。\n", sizeof(str), len, str[0], str[len - 1]);
+
+    return 0;
+}
