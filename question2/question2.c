608a86204542a877b150b647a71c4bd0caa0fafa
Akihiko-Machii <Akihiko_Machii@sis2.saison.co.jp>
2024-10-07 09:19:04 +0900

    chore: clang-formatを追加する

--- question2/question2.c
+++ question2/question2.c
@@ -2,7 +2,9 @@
 
 int main(void)
 {
-    printf("char=%05d short=%05d int=%05d long=%05d float=%05d double=%05d\n", sizeof(char), sizeof(short), sizeof(int), sizeof(long), sizeof(float), sizeof(double));
+    printf("char=%05d short=%05d int=%05d long=%05d float=%05d double=%05d\n",
+           sizeof(char), sizeof(short), sizeof(int), sizeof(long),
+           sizeof(float), sizeof(double));
 
     return 0;
 }
