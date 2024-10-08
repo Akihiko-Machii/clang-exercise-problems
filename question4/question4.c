d58b2135a6139c006637848de548b0c3afc58456
Akihiko-Machii <Akihiko_Machii@sis2.saison.co.jp>
2024-10-08 18:06:46 +0900

    style: printfを追加する

--- question4/question4.c
+++ question4/question4.c
@@ -2,12 +2,13 @@
 
 int main(void)
 {
-  int *p = NULL;
-  int i = 20;
+    int *p = NULL;
+    int i = 20;
 
-  p = &i;
+    p = &i;
 
-  printf("i:アドレス=0x%x 値=%d\np:アドレス=0x%x 値=0x%x 指す先の値=%d\n", &i, i, &p, p, *p);
+    printf("i:アドレス=0x%x 値=%d\n", &i, i);
+    printf("p:アドレス=0x%x 値=0x%x 指す先の値=%d\n", &p, p, *p);
 
-  return 0;
+    return 0;
 }
