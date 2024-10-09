991a6d9db91d93a87b51d92703574f0464d7ad0c
Akihiko-Machii <Akihiko_Machii@sis2.saison.co.jp>
2024-10-09 12:04:43 +0900

    style: フォーマット指定子を変更する

--- question4/question4.c
+++ question4/question4.c
@@ -7,8 +7,13 @@ int main(void)
 
     p = &i;
 
-    printf("i:アドレス=0x%x 値=%d\n", &i, i);
-    printf("p:アドレス=0x%x 値=0x%x 指す先の値=%d\n", &p, p, *p);
+    // フォーマット指定子%p
+    printf("i:アドレス=%p 値=%d\n", (void *)&i, i);
+    printf("p:アドレス=%p 値=%p 指す先の値=%d\n", (void *)&p, (void *)p, *p);
+
+    // フォーマット指定子0x%x
+    // printf("i:アドレス=0x%x 値=%d\n", &i, i);
+    // printf("p:アドレス=0x%x 値=0x%x 指す先の値=%d\n", &p, p, *p);
 
     return 0;
 }
