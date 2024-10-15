a2c30636d6ca6c8880d2b2d67132ceda7a88c3ae
Akihiko-Machii <Akihiko_Machii@sis2.saison.co.jp>
2024-10-15 09:32:40 +0900

    style: printfを1つにする

--- question6/question6.c
+++ question6/question6.c
@@ -14,21 +14,17 @@ int main(void)
 {
     SAMPLE_t sample;
 
-    printf("構造体のサイズ=%d メンバの合計サイズ=%d\n", sizeof(SAMPLE_t),
+    printf("構造体のサイズ=%d メンバの合計サイズ=%d\nchar m1 オフセット=%d "
+           "サイズ=%d\nshort m2 オフセット=%d サイズ=%d\nchar m3 オフセット=%d "
+           "サイズ=%d\nlong m4 オフセット=%d サイズ=%d\nchar m5 オフセット=%d "
+           "サイズ=%d\nvoid* m6 オフセット=%d サイズ=%d\n",
+           sizeof(SAMPLE_t),
            (sizeof(sample.m1) + sizeof(sample.m2) + sizeof(sample.m3) +
-            sizeof(sample.m4) + sizeof(sample.m5) + sizeof(sample.m6)));
-    printf("char m1 オフセット=%d サイズ=%d\n", offsetof(SAMPLE_t, m1),
-           sizeof(sample.m1));
-    printf("short m2 オフセット=%d サイズ=%d\n", offsetof(SAMPLE_t, m2),
-           sizeof(sample.m2));
-    printf("char m3 オフセット=%d サイズ=%d\n", offsetof(SAMPLE_t, m3),
-           sizeof(sample.m3));
-    printf("long m4 オフセット=%d サイズ=%d\n", offsetof(SAMPLE_t, m4),
-           sizeof(sample.m4));
-    printf("char m5 オフセット=%d サイズ=%d\n", offsetof(SAMPLE_t, m5),
-           sizeof(sample.m5));
-    printf("void* m6 オフセット=%d サイズ=%d\n", offsetof(SAMPLE_t, m6),
-           sizeof(sample.m6));
+            sizeof(sample.m4) + sizeof(sample.m5) + sizeof(sample.m6)),
+           offsetof(SAMPLE_t, m1), sizeof(sample.m1), offsetof(SAMPLE_t, m2),
+           sizeof(sample.m2), offsetof(SAMPLE_t, m3), sizeof(sample.m3),
+           offsetof(SAMPLE_t, m4), sizeof(sample.m4), offsetof(SAMPLE_t, m5),
+           sizeof(sample.m5), offsetof(SAMPLE_t, m6), sizeof(sample.m6));
 
     return 0;
 }
