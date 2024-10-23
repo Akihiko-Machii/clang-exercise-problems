297005172835c94c2c5c9ba4e6625d429a47153f
Akihiko-Machii <akihiko_machii@saison-technology.com>
2024-10-23 03:06:37 +0000

    Apply clang-format changes

--- question12/question12.c
+++ question12/question12.c
@@ -2,47 +2,39 @@
 
 int main(int argc, char *argv[])
 {
-  if (argc < 2)
-  {
-    printf("コマンドラインで./a.outの後にusageを指定してください\n");
-    return 1;
-  }
-
-  FILE *fp;
-  char filename[256];
-  int c;
-  int newline = 1;
-
-  snprintf(filename, sizeof(filename), "%s.txt", argv[1]);
-  fp = fopen(filename, "r");
-
-  if (fp == NULL)
-  {
-    printf("ファイルを開けませんでした\n");
-    return 1;
-  }
-
-  while ((c = fgetc(fp)) != EOF)
-  {
-    if (newline)
-    {
-      printf("ADDRESS: ");
-      newline = 0;
+    if (argc < 2) {
+        printf("コマンドラインで./a.outの後にusageを指定してください\n");
+        return 1;
     }
 
-    if (c == '\n')
-    {
-      printf("\n");
-      newline = 1;
-    }
-    else
-    {
+    FILE *fp;
+    char filename[256];
+    int c;
+    int newline = 1;
 
-      printf("%02X", (unsigned char)c);
-    }
-  }
+    snprintf(filename, sizeof(filename), "%s.txt", argv[1]);
+    fp = fopen(filename, "r");
 
-  fclose(fp);
-  return 0;
+    if (fp == NULL) {
+        printf("ファイルを開けませんでした\n");
+        return 1;
+    }
+
+    while ((c = fgetc(fp)) != EOF) {
+        if (newline) {
+            printf("ADDRESS: ");
+            newline = 0;
+        }
+
+        if (c == '\n') {
+            printf("\n");
+            newline = 1;
+        } else {
+
+            printf("%02X", (unsigned char)c);
+        }
+    }
+
+    fclose(fp);
+    return 0;
 }
-
