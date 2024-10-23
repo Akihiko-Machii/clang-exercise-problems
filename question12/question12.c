3ba9313ef3211d0ed310216ac383f3f9e666700c
Akihiko-Machii <akihiko_machii@saison-technology.com>
2024-10-23 12:05:48 +0900

    feat: question12を追加する

---
+++ question12/question12.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+  if (argc < 2)
+  {
+    printf("コマンドラインで./a.outの後にusageを指定してください\n");
+    return 1;
+  }
+
+  FILE *fp;
+  char filename[256];
+  int c;
+  int newline = 1;
+
+  snprintf(filename, sizeof(filename), "%s.txt", argv[1]);
+  fp = fopen(filename, "r");
+
+  if (fp == NULL)
+  {
+    printf("ファイルを開けませんでした\n");
+    return 1;
+  }
+
+  while ((c = fgetc(fp)) != EOF)
+  {
+    if (newline)
+    {
+      printf("ADDRESS: ");
+      newline = 0;
+    }
+
+    if (c == '\n')
+    {
+      printf("\n");
+      newline = 1;
+    }
+    else
+    {
+
+      printf("%02X", (unsigned char)c);
+    }
+  }
+
+  fclose(fp);
+  return 0;
+}
+
